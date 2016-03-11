35c570f2064ca45e29a18d26ff78aec6b05525eb
leithdm@gmail.com <darrenm.leith@lethalapps.com>
2016-03-11 10:19:31 +0000

    docs: First commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Darren Leith on 11/03/2016.
+//  Copyright © 2016 Darren Leith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
